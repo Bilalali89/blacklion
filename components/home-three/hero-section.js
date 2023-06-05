895c04ab332b659e9ce14a34359e165a85ab4008
Bilalali89 <bilalali89@gmail.com>
2023-06-05 12:35:56 +0500

    overflow fix

--- components/home-three/hero-section.js
+++ components/home-three/hero-section.js
@@ -1,10 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { MotionConfig, motion, easeIn} from "framer-motion";
-
+import { MotionConfig, motion } from "framer-motion";
 
 export default function HeroSection() {
-  const eased = easeIn(0.5)
 
   return (
     <div className="fugu--hero-section home-hero-one">
@@ -12,52 +10,76 @@ export default function HeroSection() {
       <div className="container">
         <div className="row">
           <div
-            className="col-lg-4 d-flex justify-content-center"
-            style={{ flexDirection: "column" }}
+            className="col-lg-4 d-flex justify-content-center flex-direction-column"
           >
             <div className="fugu--hero-content">
               <h1>
-                <MotionConfig transition={{duration: 1, delay: 0}}>
-                  <motion.div initial={{ opacity: 0, x:"-30%"}}
-                animate={{ opacity: 1, x:"0%" }}>Enhanced</motion.div>
+                <MotionConfig transition={{ duration: 1, delay: 0 }}>
+                  <motion.div
+                    initial={{ opacity: 0, x: "-30%" }}
+                    animate={{ opacity: 1, x: "0%" }}
+                  >
+                    Enhanced
+                  </motion.div>
                 </MotionConfig>
-                <MotionConfig transition={{duration: 1, delay: 0.5}}>
-                  <motion.div initial={{ opacity: 0, x:"-30%"}}
-                animate={{ opacity: 1, x:"0%" }}>Music</motion.div>
-                </MotionConfig><MotionConfig transition={{duration: 1, delay: 1}}>
-                  <motion.div initial={{ opacity: 0, x:"-30%"}}
-                animate={{ opacity: 1, x:"0%" }}>Valuation</motion.div>
+                <MotionConfig transition={{ duration: 1, delay: 0.5 }}>
+                  <motion.div
+                    initial={{ opacity: 0, x: "-30%" }}
+                    animate={{ opacity: 1, x: "0%" }}
+                  >
+                    Music
+                  </motion.div>
                 </MotionConfig>
-                <MotionConfig transition={{duration: 1.2, delay: 1.5}}>
-                  <motion.div  initial={{ opacity: 0, x:"-30%"}}
-                animate={{ opacity: 1, x:"0%" }}><span className="text">with</span> AI</motion.div>
+                <MotionConfig transition={{ duration: 1, delay: 1 }}>
+                  <motion.div
+                    initial={{ opacity: 0, x: "-30%" }}
+                    animate={{ opacity: 1, x: "0%" }}
+                  >
+                    Valuation
+                  </motion.div>
+                </MotionConfig>
+                <MotionConfig transition={{ duration: 1.2, delay: 1.5 }}>
+                  <motion.div
+                    initial={{ opacity: 0, x: "-30%" }}
+                    animate={{ opacity: 1, x: "0%" }}
+                  >
+                    <span className="text">with</span> AI
+                  </motion.div>
                 </MotionConfig>
               </h1>
             </div>
           </div>
           <div className="col-lg-8">
             <div className="BL--hero-right">
-              <MotionConfig transition={{duration: 1}}>
-              <motion.span initial={{x:"-200%", rotate:"90deg"}}  animate={{x:"0%", rotate:"0deg"}} className="dollar"> <img src="/images/home/Dollar-mark.png" alt="dollar" /></motion.span>
+              <MotionConfig transition={{ duration: 1, delay: 2}}>
+                <motion.span
+                  initial={{ x: "50%", opacity: 0 }}
+                  animate={{ x: "0%", opacity: 1 }}
+                  className="dollar"
+                >
+                  {" "}
+                  <img src="/images/home/Dollar-mark.png" alt="dollar" />
+                </motion.span>
               </MotionConfig>
-           
-              <MotionConfig transition={{duration: 1}}>
-              <motion.div initial={{ opacity: 0, scale: 0, x: "100%"}}
-                animate={{ opacity: 1 , scale: 1, x: " 0%" }}>
-                {" "}
-                <img src="/images/home/Laptop-home.png" alt="laptop" />
-              </motion.div>
-                </MotionConfig>
-              
+
+              <MotionConfig transition={{ duration: 1 }}>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0, y: "100%" }}
+                  animate={{ opacity: 1, scale: 1, y: " 0%" }}
+                >
+                  {" "}
+                  <img src="/images/home/Laptop-home.png" alt="laptop" />
+                </motion.div>
+              </MotionConfig>
             </div>
             <div className="BL--hero--btn">
               <Link href={"#"} legacyBehavior>
                 {/* <a className="hero-btn">GET EARLY ACCESS</a> */}
-                
+
                 <motion.a
                   className="hero-btn"
                   whileHover={{ scale: 1.1 }}
-                  transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 20 }}
                 >
                   GET EARLY ACCESS
                 </motion.a>
