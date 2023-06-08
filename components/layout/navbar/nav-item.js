6055be2db2770042d1db74141bf0da16ee5479d1
Bilalali89 <bilalali89@gmail.com>
2023-06-08 16:14:12 +0500

    GSAP bug fix

--- components/layout/navbar/nav-item.js
+++ components/layout/navbar/nav-item.js
@@ -6,32 +6,36 @@ import MegaMenu from "./mega-menu";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function NavItem({ pathSite, navItemText, menuItems, megaMenu = false }) {
+export default function NavItem({
+  pathSite,
+  navItemText,
+  menuItems,
+  megaMenu = false,
+}) {
+  const [isOpenDropDown, setIsOpenDropDown] = useState(false);
+  const handleClick = (e) => {
+    setIsOpenDropDown(!isOpenDropDown);
+  };
 
-	const [isOpenDropDown, setIsOpenDropDown] = useState(false);
-	const handleClick = (e) => {
-		setIsOpenDropDown(!isOpenDropDown);
-	};
+  const router = useRouter();
 
-	const router = useRouter()
+  return (
+    <li className={`nav-item ${menuItems ? "nav-item-has-children" : ""}`}>
+      <a
+        href={pathSite}
+        className={`nav-link-item cursor-scale drop-trigger ${
+          router.pathname === pathSite ? "active" : ""
+        }`}
+        onClick={handleClick}
+      >
+        {navItemText}
+      </a>
 
-	return (
-		<li className={`nav-item ${menuItems ? "nav-item-has-children" : ""}`}>
-			
-			{/* <a href={{pathname: `${pathSite}`}} className={`nav-link-item drop-trigger ${router.pathname === pathSite ? 'active' : ''}`} onClick={handleClick}
-			>
-				{navItemText}
-			</a> */}
-			
-			<a href={pathSite} className={`nav-link-item drop-trigger ${router.pathname === pathSite ? 'active' : ''}`} onClick={handleClick}>
-				{navItemText}
-			</a>
-
-			{!megaMenu ? (
-				<DropdownMenu isOpenDropDown={isOpenDropDown} menuItems={menuItems} />
-			) : (
-				<MegaMenu menuItems={menuItems} />
-			)}
-		</li>
-	);
+      {!megaMenu ? (
+        <DropdownMenu isOpenDropDown={isOpenDropDown} menuItems={menuItems} />
+      ) : (
+        <MegaMenu menuItems={menuItems} />
+      )}
+    </li>
+  );
 }
